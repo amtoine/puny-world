eb3ce1bd9168756fe8b05ca62a4708643cf86ebc
amtoine <stevan.antoine@gmail.com>
2024-07-06 18:14:24 +0200

    complete main

--- main.py
+++ main.py
@@ -56,6 +56,9 @@ def load_tileset(tileset: Path) -> (List[dict], list):
 if __name__ == "__main__":
     tiles, _ = load_tileset(Path("metadata/punyworld-overworld-tiles.json"))
 
+    # "show indices"
+    t, n, e, s, w = (0, 0, 0, 0, 0)
+
     pygame.init()
     screen = pygame.display.set_mode((1280, 720))
     clock = pygame.time.Clock()
@@ -64,18 +67,88 @@ if __name__ == "__main__":
 
     running = True
     while running:
+        # compute its neighbours
+        tile = tiles[list(tiles.keys())[t]]
+        tile = {
+            'n': [k for k, v in tiles.items() if v.south == tile.north and tile.north is not None],
+            'e': [k for k, v in tiles.items() if v.west == tile.east and tile.east is not None],
+            's': [k for k, v in tiles.items() if v.north == tile.south and tile.south is not None],
+            'w': [k for k, v in tiles.items() if v.east == tile.west and tile.west is not None],
+        }
         for event in pygame.event.get():
             if event.type == pygame.QUIT or (
                 event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
             ):
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                # update the "show indices"
+                if event.key == pygame.K_j:
+                    if event.mod & pygame.KMOD_LSHIFT:
+                        if len(tile['s']) > 0:
+                            s = (s - 1) % len(tile['s'])
+                    else:
+                        if len(tile['s']) > 0:
+                            s = (s + 1) % len(tile['s'])
+                elif event.key == pygame.K_k:
+                    if event.mod & pygame.KMOD_LSHIFT:
+                        if len(tile['n']) > 0:
+                            n = (n - 1) % len(tile['n'])
+                    else:
+                        if len(tile['n']) > 0:
+                            n = (n + 1) % len(tile['n'])
+                elif event.key == pygame.K_h:
+                    if event.mod & pygame.KMOD_LSHIFT:
+                        if len(tile['w']) > 0:
+                            w = (w - 1) % len(tile['w'])
+                    else:
+                        if len(tile['w']) > 0:
+                            w = (w + 1) % len(tile['w'])
+                elif event.key == pygame.K_l:
+                    if event.mod & pygame.KMOD_LSHIFT:
+                        if len(tile['e']) > 0:
+                            e = (e - 1) % len(tile['e'])
+                    else:
+                        if len(tile['e']) > 0:
+                            e = (e + 1) % len(tile['e'])
+                elif event.key == pygame.K_SPACE:
+                    if event.mod & pygame.KMOD_LSHIFT:
+                        t = (t - 1) % len(tiles.keys())
+                        n, e, s, w = (0, 0, 0, 0)
+                        print(list(tiles.keys())[t])
+                    else:
+                        t = (t + 1) % len(tiles.keys())
+                        n, e, s, w = (0, 0, 0, 0)
+                        print(list(tiles.keys())[t])
 
         screen.fill((0, 0, 0))
-        w, h = screen.get_size()
+
+        # show the tile and its neighbours
+        width, height = screen.get_size()
         screen.blit(
-            pygame.transform.scale(tiles["grass_2"].image, (128, 128)),
-            (w / 2, h / 2),
+            pygame.transform.scale(tiles[list(tiles.keys())[t]].image, (128, 128)),
+            (width / 2, height / 2),
         )
+        if len(tile['n']) > 0:
+            screen.blit(
+                pygame.transform.scale(tiles[tile['n'][n]].image, (128, 128)),
+                (width / 2, height / 2 - 128),
+            )
+        if len(tile['s']) > 0:
+            screen.blit(
+                pygame.transform.scale(tiles[tile['s'][s]].image, (128, 128)),
+                (width / 2, height / 2 + 128),
+            )
+        if len(tile['w']) > 0:
+            screen.blit(
+                pygame.transform.scale(tiles[tile['w'][w]].image, (128, 128)),
+                (width / 2 - 128, height / 2),
+            )
+        if len(tile['e']) > 0:
+            screen.blit(
+                pygame.transform.scale(tiles[tile['e'][e]].image, (128, 128)),
+                (width / 2 + 128, height / 2),
+            )
+
         pygame.display.flip()
         dt = clock.tick(frame_rate) / 1000
 
