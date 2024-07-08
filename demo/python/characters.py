9810180e90f185c9de8ff6c7996dfd685a17f3d1
amtoine <stevan.antoine@gmail.com>
2024-07-08 19:41:45 +0200

    adapt "character" script to generate GIFs

--- demo/python/characters.py
+++ demo/python/characters.py
@@ -1,6 +1,10 @@
 import pygame
 from pathlib import Path
 from tileset import load_tileset
+from PIL import Image
+import numpy as np
+import argparse
+import os
 
 RENDERING_GRID_WIDTH = 8
 RENDERING_TILE_SIZE = 128
@@ -18,13 +22,10 @@ BLACK = (0, 0, 0)
 ANIMATION_INV_SPEED = 15
 
 
-if __name__ == "__main__":
+def run(characters):
     pygame.init()
     screen = pygame.display.set_mode(CANVA_SIZE)
     clock = pygame.time.Clock()
-    dt = 0
-
-    _, _, characters = load_tileset(Path("../../punyworld.json"))
 
     curr = 0
     print(list(characters.keys())[curr])
@@ -60,7 +61,63 @@ if __name__ == "__main__":
             )
 
         pygame.display.flip()
-        dt = clock.tick(FRAME_RATE) / NB_MS_IN_SEC
+        clock.tick(FRAME_RATE)
         t += 1
 
     pygame.quit()
+
+
+def generate_gif(characters, output_dir: str):
+    screen = pygame.surface.Surface(CANVA_SIZE)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for name, character in characters.items():
+        frames = []
+        print(f"recording frames for {name}... ", end='', flush=True)
+
+        for t in range(100):
+            screen.fill(BLACK)
+
+            for i, (k, v) in enumerate(character.items()):
+                screen.blit(
+                    pygame.transform.scale(
+                        v[t // ANIMATION_INV_SPEED % len(v)],
+                        (RENDERING_TILE_SIZE, RENDERING_TILE_SIZE),
+                    ),
+                    (
+                        (i % RENDERING_GRID_WIDTH) * RENDERING_TILE_SIZE,
+                        (i // RENDERING_GRID_WIDTH) * RENDERING_TILE_SIZE,
+                    ),
+                )
+
+            frames.append(np.transpose(pygame.surfarray.array3d(screen), (1, 0, 2)))
+
+        print("done")
+        print("generating GIF... ", end='', flush=True)
+        imgs = [Image.fromarray(img) for img in frames]
+        output = f"{output_dir}/{name}.gif"
+        imgs[0].save(
+            output,
+            save_all=True,
+            append_images=imgs[1:],
+            duration=1_000/FRAME_RATE,
+            loop=0,
+        )
+        print("done")
+        print(f"GIF written in {output}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--generate-gif", "-g", action="store_true")
+    parser.add_argument("--output-dir", "-o", type=str, default="out/")
+    args = parser.parse_args()
+
+    _, _, characters = load_tileset(Path("../../punyworld.json"))
+
+    if not args.generate_gif:
+        run(characters)
+    else:
+        generate_gif(characters, args.output_dir)
