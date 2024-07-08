84c494233dd487d57e982fbe6486e191cee1ab97
amtoine <stevan.antoine@gmail.com>
2024-07-08 19:16:50 +0200

    adapt "animation" script to generate GIF

--- demo/python/animation.py
+++ demo/python/animation.py
@@ -1,6 +1,9 @@
 import pygame
 from pathlib import Path
 from tileset import load_tileset, get_tile, get_animation_steps
+from PIL import Image
+import numpy as np
+import argparse
 
 RENDERING_GRID_WIDTH = 10
 RENDERING_TILE_SIZE = 92
@@ -19,6 +22,11 @@ ANIMATION_INV_SPEED = 10
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--generate-gif", "-g", action="store_true")
+    parser.add_argument("--output", "-o", type=str, default="out.gif")
+    args = parser.parse_args()
+
     tiles, animations, _ = load_tileset(Path(TILESET))
     animated = {k: v for k, v in tiles.items() if v.animation}
 
@@ -27,21 +35,28 @@ if __name__ == "__main__":
     h = quotient + (remainder != 0)
     w = (len(animated) - remainder) // quotient
 
-    pygame.init()
-    screen = pygame.display.set_mode(
-        (w * RENDERING_TILE_SIZE, h * RENDERING_TILE_SIZE)
-    )
+    window_size = (w * RENDERING_TILE_SIZE, h * RENDERING_TILE_SIZE)
+    if not args.generate_gif:
+        pygame.init()
+        screen = pygame.display.set_mode(window_size)
+    else:
+        screen = pygame.surface.Surface(window_size)
+        frames = []
     clock = pygame.time.Clock()
     dt = 0
+
+    if args.generate_gif:
+        print("recording frames... ", end='')
 
     t = 0
     running = True
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT or (
-                event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
-            ):
-                running = False
+        if not args.generate_gif:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT or (
+                    event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+                ):
+                    running = False
 
         screen.fill(BLACK)
 
@@ -60,8 +75,27 @@ if __name__ == "__main__":
                 ),
             )
 
-        pygame.display.flip()
-        dt = clock.tick(FRAME_RATE) / NB_MS_IN_SEC
+        if args.generate_gif:
+            frames.append(np.transpose(pygame.surfarray.array3d(screen), (1, 0, 2)))
+            if t > 4 * ANIMATION_INV_SPEED:
+                break
+        else:
+            pygame.display.flip()
+            dt = clock.tick(FRAME_RATE) / NB_MS_IN_SEC
         t += 1
+
+    if args.generate_gif:
+        print("done")
+        print("generating GIF... ", end='', flush=True)
+        imgs = [Image.fromarray(img) for img in frames]
+        imgs[0].save(
+            args.output,
+            save_all=True,
+            append_images=imgs[1:],
+            duration=1_000/FRAME_RATE,
+            loop=True,
+        )
+        print("done")
+        print(f"GIF written in {args.output}")
 
     pygame.quit()
